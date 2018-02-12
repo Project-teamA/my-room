d9b5f893ea12b4b05cbffd3b4cb5241b4b394148
zhangjing1301 <34084950+zhangjing1301@users.noreply.github.com>
2018-02-12 23:06:10 +0900

    妖怪と式神アニメション
    妖怪と式神アニメション

---
+++ project-A/anime/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	Animator animator ;
+	
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+
+
+	void OnGUI()
+	{
+		if(GUILayout.Button("金式神攻撃"))
+		{
+			animator.Play("金式神1");
+		}
+    }
+}
